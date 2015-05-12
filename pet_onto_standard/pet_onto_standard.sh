295179046127ae9c351350648828379772bd1b9d
Ivan Gonzalez <iglpdc@gmail.com>
2015-05-12 11:34:54 -0400

    Use a variable for the flirt input file

--- pet_onto_standard/pet_onto_standard.sh
+++ pet_onto_standard/pet_onto_standard.sh
@@ -15,18 +15,19 @@ mkdir -p ${DESTINATION_DIR}
 cp ${ORIGIN_DIR}/${SUBJECT_RECON}.nii ${DESTINATION_DIR}/${SUBJECT_RECON}.nii
 cd ${DESTINATION_DIR}
 
-##compute transformation matrix for linear registration of SUV with fs T1, with spm
+# Compute transformation matrix for linear registration of SUV with fs T1, with spm
 spmregister --mov ${SUBJECT_RECON}.nii --s ${SUBJECT_ID} --reg SUV.lin_T1.dat --fsvol orig
- 
-##to CHECK REGISTRATION 
+
+# to CHECK REGISTRATION 
 tkregister2 --mov ${SUBJECT_RECON}.nii --targ ${SUBJECT_DIR}/mri/orig.mgz --reg SUV.lin_T1.dat --surf orig
 
-##use tkergister to transform the matrix into .mat, and then invert the transformation matrix 
-tkregister2 --mov ${SUBJECT_RECON}.nii --targ ${SUBJECT_DIR}/mri/brainmask.mgz --reg SUV.lin_T1.dat --fslregout SUV.lin_T1.mat --noedit 
+# Use tkergister to transform the matrix into .mat, and then invert the transformation matrix
+tkregister2 --mov ${SUBJECT_RECON}.nii --targ ${SUBJECT_DIR}/mri/brainmask.mgz --reg SUV.lin_T1.dat --fslregout SUV.lin_T1.mat --noedit
 convert_xfm -omat T1_lin_SUV.mat -inverse SUV.lin_T1.mat
 
-##apply T1 to SUV registration
-##convert various files and reorient to standard to match MNI/fsl coordinates system
+######################## Apply T1 to SUV registration ##########################
+
+# Convert various files and reorient to standard to match MNI/fsl coordinates system
 mri_convert ${SUBJECT_DIR}/mri/brainmask.mgz brainmask.nii
 mri_convert ${SUBJECT_DIR}/mri/T1.mgz T1.nii
 mri_binarize --i ${SUBJECT_DIR}/mri/aparc+aseg.mgz --min 0.5 --dilate 2 --o aparc+aseg_BIN.nii 
@@ -46,18 +47,22 @@ flirt -noresample -in ${SUBJECT_RECON}.nii -ref T1-2mm_skullstripped.nii -applyx
 fslreorient2std ${SUBJECT_RECON}.lin_T1 ${SUBJECT_RECON}.lin_T1_orientOK
 fslmaths ${SUBJECT_RECON}.lin_T1_orientOK -mas aparc+aseg_BIN-2mm_orientOK ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped
 
-#### flirt freesurfer mprage (T1-2mm_skullstripped) to MNI, to obtain the T1_lin_MNI152.mat matrix
-flirt -ref T1-2mm_skullstripped_orientOK -in /autofs/space/lyon_006/pubsw/Linux2-2.3-x86_64/packages/fsl.64bit/4.1.4/data/standard/MNI152_T1_2mm_brain.nii.gz -omat MNI152_lin_T1.mat 
+# Find the input file for flirt
+setenv FLIRT_DATADIR /autofs/space/lyon_006/pubsw/Linux2-2.3-x86_64/packages/fsl.64bit/4.1.4/
+setenv MNI152_T1_2MM_BRAIN_NII $FLIRT_DATADIR/data/standard/MNI152_T1_2mm_brain.nii.gz
+
+# flirt freesurfer mprage (T1-2mm_skullstripped) to MNI, to obtain the T1_lin_MNI152.mat matrix
+flirt -ref T1-2mm_skullstripped_orientOK -in $MNI152_T1_2MM_BRAIN_NII -omat MNI152_lin_T1.mat 
 convert_xfm -omat T1_lin_MNI152.mat -inverse MNI152_lin_T1.mat
 
-###NONLINEAR REGISTRATION OF T1 to MNI
-fnirt --ref=/autofs/space/lyon_006/pubsw/Linux2-2.3-x86_64/packages/fsl.64bit/4.1.4/data/standard/MNI152_T1_2mm.nii.gz --in=T1-2mm_orientOK.nii --aff=T1_lin_MNI152.mat --cout=T1_nl_MNI152.reg --config=T1_2_MNI152_2mm.cnf --iout=T1_nl_MNI152.nii
+# Apply non-linear registration of T1 to MNI
+fnirt --ref=$MNI152_T1_2MM_BRAIN_NII --in=T1-2mm_orientOK.nii --aff=T1_lin_MNI152.mat --cout=T1_nl_MNI152.reg --config=T1_2_MNI152_2mm.cnf --iout=T1_nl_MNI152.nii
 
-###APPLY FNIRT MATRIX TO SUV
-applywarp --ref=/autofs/space/lyon_006/pubsw/Linux2-2.3-x86_64/packages/fsl.64bit/4.1.4/data/standard/MNI152_T1_2mm.nii.gz --in=${SUBJECT_RECON}.lin_T1_orientOK_skullstripped --warp=T1_nl_MNI152.reg --out=${SUBJECT_RECON}.nl_MNI152
+# Apply fnirt matrix to SUV
+applywarp --ref=$MNI152_T1_2MM_BRAIN_NII --in=${SUBJECT_RECON}.lin_T1_orientOK_skullstripped --warp=T1_nl_MNI152.reg --out=${SUBJECT_RECON}.nl_MNI152
 
-###SPATIAL SMOOTHING
-#FOR SPATIAL SMOOTHING, sigma=FWHM/2.355 ==> for 6mm is 2.55; http://mathworld.wolfram.com/GaussianFunction.html
+########################### Spatial smoothing #############################
+# sigma=FWHM/2.355 ==> for 6mm is 2.55; See http://mathworld.wolfram.com/GaussianFunction.html
 
 ## 6mm smoothing of PET in MNI
 fslmaths ${SUBJECT_RECON}.nl_MNI152 -kernel gauss 2.55 -fmean ${SUBJECT_RECON}.nl_MNI152_sm6mm -odt float
@@ -68,12 +73,12 @@ fslmaths ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped -kernel gauss 1.27 -fmea
 ## 6mm smoothing, for visualization of subject-level PET in subject space with FSL
 fslmaths ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped -kernel gauss 2.55 -fmean ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_sm6mm -odt float
 
-### NORMALIZATION
-## normalization and smoothing of PET in MNI
+########################### Normalization #############################
+# Normalization and smoothing of PET in MNI
 fslmaths ${SUBJECT_RECON}.nl_MNI152 -mas ${FSL_DIR}/data/standard/MNI152_T1_2mm_brain_mask.nii.gz -inm 1 ${SUBJECT_RECON}.nl_MNI152_norm -odt float
 
 fslmaths ${SUBJECT_RECON}.nl_MNI152_norm -kernel gauss 2.55 -fmean ${SUBJECT_RECON}.nl_MNI152_norm_sm6mm -odt float
 
-## normalization and smoothing of lin_T1_orientOK_skullstripped
+# Normalization and smoothing of lin_T1_orientOK_skullstripped
 fslmaths ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped -mas aparc+aseg_BIN-2mm_orientOK -inm 1 ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_norm -odt float
 fslmaths ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_norm -kernel gauss 2.55 -fmean ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_norm_sm6mm -odt float
