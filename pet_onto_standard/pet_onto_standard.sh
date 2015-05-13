854634db3bc3f642af0cfa2592d68b7299003044
Ivan Gonzalez <iglpdc@gmail.com>
2015-05-13 12:57:30 -0400

    Use bash instead of tcsh :-)
    - Add checks for the various parameters
    - Add an "usage function"
    - Add success meesage at the end

--- pet_onto_standard/pet_onto_standard.sh
+++ pet_onto_standard/pet_onto_standard.sh
@@ -1,28 +1,101 @@
-#!/usr/bin/env tcsh
-## DATA ACQUIRED ON BAY 7, no L/R flip required!!!
-## Using fslreorient2std
-## in Aether, obtain final 60-90min recon nifti (e.g. with two-step single frame TAC recon)
+#!/usr/bin/env bash
+# Transforms a PET image into MNI space and normalizes to whole brain uptake
+#
+# This is to be used with data acquired on Bay 7, so there's no L/R flip required.
+#
+# Example:
+#       ./pet_on_standard.sh /autofs/cluster/PBR/FS/subjects/SCAC64H_PBR28 ./SCAC_PBR_SUV_60-90.nii
+#
+usage()
+{
+    # Prints a help text
+    SCRIPT_NAME=$1
 
-setenv SUBJECT_ID HAYW_PBR
-setenv SUBJECT_RECON HAYW_PBR_SUV60-90
-setenv SUBJECTS_DIR /autofs/cluster/hookerlab/Users/Cristina/Huntington_FGD/FS/subjects/
-setenv SUBJECT_DIR ${SUBJECTS_DIR}/${SUBJECT_ID}
-setenv DESTINATION_DIR /autofs/cluster/hookerlab/Studies/PBR/Rosas/PET_onto_standard/${SUBJECT_ID}/PET-2-standard_2
-setenv ORIGIN_DIR /autofs/cluster/hookerlab/Studies/PBR/Rosas/PET/${SUBJECT_ID}
+    echo "Transforms a PET image into MNI space and normalizes to whole brain uptake."
+    echo
+    echo "usage: $SCRIPT_NAME [-h] pet_recon fs_recon_dir [dest_dir]"
+    echo "       pet_recon                  the reconstructed PET nifty file"
+    echo "       fs_recon_dir               the directory with the Freesurfer reconstruction"
+    echo "       dest_dir                   a directory to store the transformed image"
+    echo "       -h                         prints this text"
+    echo
+    echo "By default, dest_dir is a pet-to-standard directory inside the directory where the" 
+    echo "original image is."
+    echo
+    echo " Example:"
+    echo "  ./pet_on_standard.sh /cluster/PBR/FS/subjects/SCAC64H_PBR28 ./SCAC_PBR_SUV_60-90.nii"
+}
+
+# Process the options passed to the script
+SCRIPT_NAME=$0
+
+while getopts ':hn' opt; do
+    case "${opt}" in
+        h) usage $SCRIPT_NAME; exit 0 ;;
+        \?) echo "Invalid option"; usage $SCRIPT_NAME; exit 1 ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+# Process the arguments passed to the script
+if [[ $# < 2 || $# > 3 ]]; then
+    echo "Illegal number of parameters: must be 2 or 3"
+    echo
+    usage $SCRIPT_NAME
+    exit 1
+fi
+
+# Check for the PET recon files
+if !([ -f "$1" ]); then
+    echo "Cannot find image file $1"
+    echo
+    usage $SCRIPT_NAME
+    exit 1
+fi
+PET_RECON_DIR=$(dirname $1)
+PET_RECON=$(basename $1)
+PET_RECON=$(echo ${PET_RECON%.nii})
+
+# Check for the Freesurfer recon directory
+if !([ -d "$2" ]); then
+    echo "Cannot find the Freesurfer reconstruction directory $2"
+    echo
+    usage $SCRIPT_NAME
+    exit 1
+fi
+FREESURFER_RECON=$2
+FREESURFER_ID=$(basename ${FREESURFER_RECON})
+
+# Check the output directory exists or create it
+if [ $# < 3 ] ; then
+    DESTINATION_DIR=${PET_RECON_DIR}/pet-to-standard
+    if !([ -d "$DESTINATION_DIR" ]); then
+        mkdir $DESTINATION_DIR
+    fi
+else
+    DESTINATION_DIR=$3
+    if !([ -d "$DESTINATION_DIR" ]); then
+        echo "Cannot find destination directory $DESTINATION_DIR."
+        echo
+        usage $SCRIPT_NAME
+        exit 1
+    fi
+fi
 
 # Make dir for this subject, copy the nifti file there, and move to that directory
 mkdir -p ${DESTINATION_DIR}
-cp ${ORIGIN_DIR}/${SUBJECT_RECON}.nii ${DESTINATION_DIR}/${SUBJECT_RECON}.nii
+cp ${PET_RECON_DIR}/${PET_RECON}.nii ${DESTINATION_DIR}/${PET_RECON}.nii
 cd ${DESTINATION_DIR}
 
 # Compute transformation matrix for linear registration of SUV with fs T1, with spm
-spmregister --mov ${SUBJECT_RECON}.nii --s ${SUBJECT_ID} --reg SUV.lin_T1.dat --fsvol orig
+spmregister --mov ${PET_RECON}.nii --s ${FREESURFER_RECON} --reg SUV.lin_T1.dat --fsvol orig
 
-# to CHECK REGISTRATION 
-tkregister2 --mov ${SUBJECT_RECON}.nii --targ ${SUBJECT_DIR}/mri/orig.mgz --reg SUV.lin_T1.dat --surf orig
+# to check registration
+tkregister2 --mov ${PET_RECON}.nii --targ ${SUBJECT_DIR}/mri/orig.mgz --reg SUV.lin_T1.dat --surf orig
 
 # Use tkergister to transform the matrix into .mat, and then invert the transformation matrix
-tkregister2 --mov ${SUBJECT_RECON}.nii --targ ${SUBJECT_DIR}/mri/brainmask.mgz --reg SUV.lin_T1.dat --fslregout SUV.lin_T1.mat --noedit
+tkregister2 --mov ${PET_RECON}.nii --targ ${SUBJECT_DIR}/mri/brainmask.mgz --reg SUV.lin_T1.dat --fslregout SUV.lin_T1.mat --noedit
 convert_xfm -omat T1_lin_SUV.mat -inverse SUV.lin_T1.mat
 
 ######################## Apply T1 to SUV registration ##########################
@@ -42,43 +115,45 @@ fslreorient2std T1-2mm.nii T1-2mm_orientOK.nii
 fslmaths T1-2mm.nii -mas aparc+aseg_BIN-2mm.nii T1-2mm_skullstripped.nii
 fslmaths T1-2mm_orientOK.nii -mas aparc+aseg_BIN-2mm_orientOK.nii T1-2mm_skullstripped_orientOK.nii
 
-flirt -noresample -in ${SUBJECT_RECON}.nii -ref T1-2mm_skullstripped.nii -applyxfm -init SUV.lin_T1.mat -out ${SUBJECT_RECON}.lin_T1.nii.gz
+flirt -noresample -in ${PET_RECON}.nii -ref T1-2mm_skullstripped.nii -applyxfm -init SUV.lin_T1.mat -out ${PET_RECON}.lin_T1.nii.gz
 
-fslreorient2std ${SUBJECT_RECON}.lin_T1 ${SUBJECT_RECON}.lin_T1_orientOK
-fslmaths ${SUBJECT_RECON}.lin_T1_orientOK -mas aparc+aseg_BIN-2mm_orientOK ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped
+fslreorient2std ${PET_RECON}.lin_T1 ${PET_RECON}.lin_T1_orientOK
+fslmaths ${PET_RECON}.lin_T1_orientOK -mas aparc+aseg_BIN-2mm_orientOK ${PET_RECON}.lin_T1_orientOK_skullstripped
 
 # Find the input file for flirt
 setenv FLIRT_DATADIR /autofs/space/lyon_006/pubsw/Linux2-2.3-x86_64/packages/fsl.64bit/4.1.4/
-setenv MNI152_T1_2MM_BRAIN_NII $FLIRT_DATADIR/data/standard/MNI152_T1_2mm_brain.nii.gz
+setenv MNI152_T1_2MM_BRAIN_NII ${FLIRT_DATADIR}/data/standard/MNI152_T1_2mm_brain.nii.gz
 
 # flirt freesurfer mprage (T1-2mm_skullstripped) to MNI, to obtain the T1_lin_MNI152.mat matrix
-flirt -ref T1-2mm_skullstripped_orientOK -in $MNI152_T1_2MM_BRAIN_NII -omat MNI152_lin_T1.mat 
+flirt -ref T1-2mm_skullstripped_orientOK -in ${MNI152_T1_2MM_BRAIN_NII} -omat MNI152_lin_T1.mat 
 convert_xfm -omat T1_lin_MNI152.mat -inverse MNI152_lin_T1.mat
 
 # Apply non-linear registration of T1 to MNI
-fnirt --ref=$MNI152_T1_2MM_BRAIN_NII --in=T1-2mm_orientOK.nii --aff=T1_lin_MNI152.mat --cout=T1_nl_MNI152.reg --config=T1_2_MNI152_2mm.cnf --iout=T1_nl_MNI152.nii
+fnirt --ref=${MNI152_T1_2MM_BRAIN_NII} --in=T1-2mm_orientOK.nii --aff=T1_lin_MNI152.mat --cout=T1_nl_MNI152.reg --config=T1_2_MNI152_2mm.cnf --iout=T1_nl_MNI152.nii
 
 # Apply fnirt matrix to SUV
-applywarp --ref=$MNI152_T1_2MM_BRAIN_NII --in=${SUBJECT_RECON}.lin_T1_orientOK_skullstripped --warp=T1_nl_MNI152.reg --out=${SUBJECT_RECON}.nl_MNI152
+applywarp --ref=${MNI152_T1_2MM_BRAIN_NII} --in=${PET_RECON}.lin_T1_orientOK_skullstripped --warp=T1_nl_MNI152.reg --out=${PET_RECON}.nl_MNI152
 
 ########################### Spatial smoothing #############################
 # sigma=FWHM/2.355 ==> for 6mm is 2.55; See http://mathworld.wolfram.com/GaussianFunction.html
 
 ## 6mm smoothing of PET in MNI
-fslmaths ${SUBJECT_RECON}.nl_MNI152 -kernel gauss 2.55 -fmean ${SUBJECT_RECON}.nl_MNI152_sm6mm -odt float
+fslmaths ${PET_RECON}.nl_MNI152 -kernel gauss 2.55 -fmean ${PET_RECON}.nl_MNI152_sm6mm -odt float
 
 ## 3mm smoothing, for visualization of subject-level PET in subject space with FSL
-fslmaths ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped -kernel gauss 1.27 -fmean ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_sm3mm -odt float
+fslmaths ${PET_RECON}.lin_T1_orientOK_skullstripped -kernel gauss 1.27 -fmean ${PET_RECON}.lin_T1_orientOK_skullstripped_sm3mm -odt float
 
 ## 6mm smoothing, for visualization of subject-level PET in subject space with FSL
-fslmaths ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped -kernel gauss 2.55 -fmean ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_sm6mm -odt float
+fslmaths ${PET_RECON}.lin_T1_orientOK_skullstripped -kernel gauss 2.55 -fmean ${PET_RECON}.lin_T1_orientOK_skullstripped_sm6mm -odt float
 
 ########################### Normalization #############################
 # Normalization and smoothing of PET in MNI
-fslmaths ${SUBJECT_RECON}.nl_MNI152 -mas ${FSL_DIR}/data/standard/MNI152_T1_2mm_brain_mask.nii.gz -inm 1 ${SUBJECT_RECON}.nl_MNI152_norm -odt float
+fslmaths ${PET_RECON}.nl_MNI152 -mas ${FSL_DIR}/data/standard/MNI152_T1_2mm_brain_mask.nii.gz -inm 1 ${PET_RECON}.nl_MNI152_norm -odt float
 
-fslmaths ${SUBJECT_RECON}.nl_MNI152_norm -kernel gauss 2.55 -fmean ${SUBJECT_RECON}.nl_MNI152_norm_sm6mm -odt float
+fslmaths ${PET_RECON}.nl_MNI152_norm -kernel gauss 2.55 -fmean ${PET_RECON}.nl_MNI152_norm_sm6mm -odt float
 
 # Normalization and smoothing of lin_T1_orientOK_skullstripped
-fslmaths ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped -mas aparc+aseg_BIN-2mm_orientOK -inm 1 ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_norm -odt float
-fslmaths ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_norm -kernel gauss 2.55 -fmean ${SUBJECT_RECON}.lin_T1_orientOK_skullstripped_norm_sm6mm -odt float
+fslmaths ${PET_RECON}.lin_T1_orientOK_skullstripped -mas aparc+aseg_BIN-2mm_orientOK -inm 1 ${PET_RECON}.lin_T1_orientOK_skullstripped_norm -odt float
+fslmaths ${PET_RECON}.lin_T1_orientOK_skullstripped_norm -kernel gauss 2.55 -fmean ${PET_RECON}.lin_T1_orientOK_skullstripped_norm_sm6mm -odt float
+echo "All done!"
+echo "Transformed PET image stored in ${DESTINATION_DIR}/${PET_RECON}.nii"
